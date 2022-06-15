705d27929bf836a9d804626a176677ed3cf73242
Jan Corazza <corazzajan@gmail.com>
2022-06-15 03:38:27 +0200

    izdvojio stdlib u drugi direktorij

--- src/snail.py
+++ src/snail.py
@@ -13,8 +13,10 @@ def justread(filename):
     with open(filename, 'r') as f:
         return f.read()
 
-def preprocess_rek(filename, directory, imported):
+def preprocess_rek(filename, imported):
+    directory = os.path.dirname(os.path.realpath(filename))
     filename = os.path.realpath(filename)
+    print(directory, ', ', filename)
     imported.append(filename)
     with open(filename, 'r') as f:
         src = f.read()
@@ -27,24 +29,22 @@ def preprocess_rek(filename, directory, imported):
                 import_filename = os.path.realpath(import_filename)
 
                 if import_filename not in imported:
-                    other_preprocessed = preprocess_rek(import_filename, directory, imported)
+                    other_preprocessed = preprocess_rek(import_filename, imported)
                     lines.extend(other_preprocessed)
             else:
                 lines.append(line)
         return lines
 
 
-def preprocess(filename, directory):
-    lines = preprocess_rek(filename, directory, [])
+def preprocess(filename):
+    lines = preprocess_rek(filename, [])
     return '\n'.join(lines)
 
 
 def main():
     filename = os.path.realpath(sys.argv[1])
-    directory = os.path.dirname(os.path.realpath(filename))
-
     print('=== preprocessing ===')
-    src = preprocess(filename, directory)
+    src = preprocess(filename)
     # src = justread(filename)
     program = snailparser.P(src)
 
